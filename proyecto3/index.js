b01d2a8ccb747493549472e88ccd8d03204ab24b
Vero <39389784+astilvero@users.noreply.github.com>
2023-02-09 16:43:17 -0500

    Add files via upload

---
+++ proyecto3/index.js
@@ -0,0 +1,2 @@
+const lista=["Veronica", 25, true, "30-03-1998", libro={titulo:"Doctor sueño", autor:"Steven King", fecha:"2013", ulr:"https://www.pdfdrive.com/doctor-sue%C3%B1o-e58362182.html"}]
+console.log(lista)
